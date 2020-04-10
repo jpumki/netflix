2dbd4a45f16f62ca52176a25ee316782d4b2805f
jpumki <59200868+jpumki@users.noreply.github.com>
2020-04-10 17:00:22 -0400

    Updates

---
+++ precache-manifest.b057b40671a5755fb7e0adf23a4a935c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4280e145ec379f59d9603be5b0621b32",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "4a2e7786b84fa79a6fa7",
+    "url": "/netflix/static/js/2.63e15d88.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/netflix/static/js/2.63e15d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7edf22637246bb1fa8",
+    "url": "/netflix/static/js/main.0f0d5f1d.chunk.js"
+  },
+  {
+    "revision": "e33c87b0155ae3258c92",
+    "url": "/netflix/static/js/runtime-main.d0c6c79c.js"
+  },
+  {
+    "revision": "ae487a1b04b4d222ecc900003ae132a7",
+    "url": "/netflix/static/media/noPosterSmall.ae487a1b.png"
+  }
+]);
